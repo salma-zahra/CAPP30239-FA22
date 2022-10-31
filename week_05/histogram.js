5e5df840481d9571fd8e6d0115897966db139825
Salma Zahra <salmaz@uchicago.edu>
2022-10-31 16:56:18 -0500

    code for class on thursday

--- week_05/histogram.js
+++ week_05/histogram.js
@@ -30,22 +30,24 @@ d3.json('climate-jan.json').then((data) => {
     .thresholds(10)
     .value(d => d.average)(data);
 
+  consolelog(binsnn)
+
   let g = binGroups.selectAll("g")
     .data(bins)
     .join("g");
 
-  // g.append("rect")
-  //   .attr("x", d => x(d.x0) + (padding / 2))
-  //   .attr("y", d => y(d.length))
-  //   .attr("width", d => x(d.x1) - x(d.x0) - padding)
-  //   .attr("height", d => height - margin.bottom - y(d.length))
-  //   .attr("fill", "steelblue");
+  g.append("rect")
+    .attr("x", d => x(d.x0) + (padding / 2))
+    .attr("y", d => y(d.length))
+    .attr("width", d => x(d.x1) - x(d.x0) - padding)
+    .attr("height", d => height - margin.bottom - y(d.length))
+    .attr("fill", "steelblue");
 
-  // g.append("text")
-  //   .text(d => d.length)
-  //   .attr("x", d => x(d.x0) + (x(d.x1) - x(d.x0)) / 2)
-  //   .attr("y", d => y(d.length) - 5)
-  //   .attr("text-anchor", "middle")
-  //   .attr("fill", "#333");
+  g.append("text")
+    .text(d => d.length)
+    .attr("x", d => x(d.x0) + (x(d.x1) - x(d.x0)) / 2)
+    .attr("y", d => y(d.length) - 5)
+    .attr("text-anchor", "middle")
+    .attr("fill", "#333");
 
 });
