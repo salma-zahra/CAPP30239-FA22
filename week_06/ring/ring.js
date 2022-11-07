d70339f267fb5282c61fc0191a7d4add64305e25
Salma Zahra <salmaz@uchicago.edu>
2022-11-06 19:19:46 -0600

    class

--- week_06/ring/ring.js
+++ week_06/ring/ring.js
@@ -1,7 +1,7 @@
 d3.json('budget-2022.json').then((data) => {
     const height = 400,
       width = 600,
-      innerRadius = 125,
+      innerRadius = 125, // if i set my innerradius to 0 then i'll get a closed pie
       outerRadius = 175,
       labelRadius = 200;
   
@@ -17,37 +17,37 @@ d3.json('budget-2022.json').then((data) => {
       .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
   
     svg.append("g")
-      .attr("stroke", "white")
+      .attr("stroke", "white") // for the white space between the pie segments
       .attr("stroke-width", 2)
       .attr("stroke-linejoin", "round")
       .selectAll("path")
       .data(arcs)
       .join("path")
-      .attr("fill", (d, i) => d3.schemeCategory10[i])
-      .attr("d", arc);
+      .attr("fill", (d, i) => d3.schemeCategory10[i]) // this is a color scheme
+      .attr("d", arc); 
   
-    // svg.append("g")
-    //   .attr("font-size", 10)
-    //   .attr("text-anchor", "middle")
-    //   .selectAll("text")
-    //   .data(arcs)
-    //   .join("text")
-    //   .attr("transform", d => `translate(${arcLabel.centroid(d)})`)
-    //   .selectAll("tspan")
-    //   .data(d => {
-    //     return [d.data.category, d.data.amount];
-    //   })
-    //   .join("tspan")
-    //   .attr("x", 0)
-    //   .attr("y", (d, i) => `${i * 1.1}em`)
-    //   .attr("font-weight", (d, i) => i ? null : "bold")
-    //   .text(d => d);
+    svg.append("g")
+      .attr("font-size", 10)
+      .attr("text-anchor", "middle")
+      .selectAll("text")
+      .data(arcs)
+      .join("text")
+      .attr("transform", d => `translate(${arcLabel.centroid(d)})`)
+      .selectAll("tspan")
+      .data(d => {
+        return [d.data.category, d.data.amount];
+      })
+      .join("tspan")
+      .attr("x", 0)
+      .attr("y", (d, i) => `${i * 1.1}em`)
+      .attr("font-weight", (d, i) => i ? null : "bold")
+      .text(d => d);
   
-    // svg.append("text")
-    //   .attr("font-size", 30)
-    //   .attr("font-weight", "bold")
-    //   .attr("text-anchor", "middle")
-    //   .attr("alignment-baseline", "middle")
-    //   .text("2022")
-    //   .style("font-size", 20);
+    svg.append("text")
+      .attr("font-size", 30)
+      .attr("font-weight", "bold")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .text("2022")
+      .style("font-size", 20);
   });
